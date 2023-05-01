b345468d3d29c2cc8f40342700f7d661a7b19c88
bhushikha <shikhamishrabhu21@gmail.com>
2023-05-01 13:12:49 +0530

    Task 11 Node

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const http = require('http');
 
-http.createServer((req, res) => {
-  res.write('My name is Shikha!');
-  res.end();
-}).listen(4000, () => {
-  console.log('Server running at http://localhost:4000/');
+const server=http.createServer((req, res) => {
+    if (req.url === '/home') {
+        res.write('Welcome home');
+        res.end();
+    } else if (req.url === '/about') {
+        res.write('Welcome to About Us page');
+        res.end();
+    } else if (req.url === '/node') {
+        res.write('Welcome to my Node Js project -in node js');
+        res.end();
+    } else {
+        res.write('404 Not Found');
+        res.end();
+    }
 });
+server.listen(4000, () => {
+    console.log('Server running at http://localhost:4000/');
+  });
+ 
+  
+  
+  
+  
+  
