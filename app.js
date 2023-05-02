f699990623adb54aaca7b7b61f76b29485b07740
bhushikha <shikhamishrabhu21@gmail.com>
2023-05-02 22:03:01 +0530

    Task 2 Express JS

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const http = require('http');
 
+const bodyParser=require('body-parser');
+
 const express=require('express');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('in the middleware!');
-    next();
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/add-product',(req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"> Add Product </button></form>');
+    
 });
-
-app.use((req,res,next)=>{
-    console.log('in the middleware!');
-   res.send('<h1>hello from express js!</h1>');
+app.postgit('/product',(req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
-
-
+  
+app.use('/',(req, res, next) => {
+    res.send('<h1>Hello from Express.js!</h1>');
+});
 
 app.listen(3000);
