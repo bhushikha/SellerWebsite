57d51687b84fd6ec88f5977fe93d3e2a0718bc7d
bhushikha <shikhamishrabhu21@gmail.com>
2023-05-03 12:55:57 +0530

    Task 3 Express js

--- app.js
+++ app.js
@@ -6,19 +6,17 @@ const express=require('express');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}))
+const adminRoutes=requires('./Routes/admin');
+const shopRoutes=requires('./Routes/shop');
 
-app.use('/add-product',(req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"> Add Product </button></form>');
-    
-});
-app.postgit('/product',(req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-  
-app.use('/',(req, res, next) => {
-    res.send('<h1>Hello from Express.js!</h1>');
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req , res , next)=>{
+    res.status(404).send('<h1> Page not found</h1>')
+
 });
 
 app.listen(3000);
