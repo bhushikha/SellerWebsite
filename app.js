d3719f870a69e35b5559301a7892fea3bf7ef865
bhushikha <shikhamishrabhu21@gmail.com>
2023-05-28 13:07:00 +0530

    Task 9 express

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-const path= require('path');
+const path = require('path');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const bodyParser=require('body-parser');
-
-const errorController=require('./controllers/error');
+const errorController = require('./controllers/error');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes=require('./Routes/admin');
-const shopRoutes=require('./Routes/shop');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3012);
+app.listen(3000);
